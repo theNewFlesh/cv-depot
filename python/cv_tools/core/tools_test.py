29d49e6440dd1456a0310089aee27c490aab3edb
Alex Braun <alexander.g.braun@gmail.com>
2025-02-01 13:43:13 -0500

    removed cruft

--- python/cv_tools/core/tools_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import cv_tools.core.tools as cct
-# ------------------------------------------------------------------------------
-
-
-class ToolsTests(unittest.TestCase):
-    def test_dummy(self):
-        cct.dummy()
