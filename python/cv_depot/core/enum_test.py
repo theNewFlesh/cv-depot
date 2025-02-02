043252d82502e5b1c7f0387043987932ead30933
Alex Braun <alexander.g.braun@gmail.com>
2025-02-02 00:17:02 +0000

    removed ImageFormat

--- python/cv_depot/core/enum_test.py
+++ python/cv_depot/core/enum_test.py
@@ -3,9 +3,7 @@ import unittest
 from lunchbox.enforce import EnforceError
 import numpy as np
 
-from cv_depot.core.enum import (
-    BitDepth, ImageFormat, ImageCodec, VideoFormat, VideoCodec
-)
+from cv_depot.core.enum import BitDepth, ImageFormat, VideoFormat, VideoCodec
 # ------------------------------------------------------------------------------
 
 
@@ -104,67 +102,6 @@ custom_metadata: True'''[1:]
         expected = 'ImageFormat not found for given extension: .foo'
         with self.assertRaisesRegex(TypeError, expected):
             ImageFormat.from_extension('.foo')
-
-
-class ImageCodecTests(unittest.TestCase):
-    def test_repr(self):
-        x = ImageCodec.PXR24
-        result = repr(x)
-        expected = '''
-<ImageCodec.PXR24>
-  string: pxr24
-exr_code: 5'''[1:]
-        self.assertEqual(result, expected)
-
-    def test_from_string(self):
-        self.assertEqual(ImageCodec.from_string('piz'), ImageCodec.PIZ)
-        self.assertEqual(ImageCodec.from_string('b44'), ImageCodec.B44)
-        self.assertEqual(ImageCodec.from_string('b44a'), ImageCodec.B44A)
-        self.assertEqual(ImageCodec.from_string('dwaa'), ImageCodec.DWAA)
-        self.assertEqual(ImageCodec.from_string('dwab'), ImageCodec.DWAB)
-        self.assertEqual(ImageCodec.from_string('pxr24'), ImageCodec.PXR24)
-        self.assertEqual(ImageCodec.from_string('rle'), ImageCodec.RLE)
-        self.assertEqual(ImageCodec.from_string('zip'), ImageCodec.ZIP)
-        self.assertEqual(ImageCodec.from_string('zips'), ImageCodec.ZIPS)
-        self.assertEqual(
-            ImageCodec.from_string('uncompressed'),
-            ImageCodec.UNCOMPRESSED
-        )
-
-    def test_from_string_errors(self):
-        expected = 'Value given is not a string. 54 !=.*str'
-        with self.assertRaisesRegex(EnforceError, expected):
-            ImageCodec.from_string(54)
-
-        expected = '"foo" has no legal ImageCodec type. '
-        expected += 'Legal codec strings: .*'
-        with self.assertRaisesRegex(EnforceError, expected):
-            ImageCodec.from_string('foo')
-
-    def test_from_exr_code(self):
-        self.assertEqual(ImageCodec.from_exr_code(4), ImageCodec.PIZ)
-        self.assertEqual(ImageCodec.from_exr_code(6), ImageCodec.B44)
-        self.assertEqual(ImageCodec.from_exr_code(7), ImageCodec.B44A)
-        self.assertEqual(ImageCodec.from_exr_code(8), ImageCodec.DWAA)
-        self.assertEqual(ImageCodec.from_exr_code(9), ImageCodec.DWAB)
-        self.assertEqual(ImageCodec.from_exr_code(5), ImageCodec.PXR24)
-        self.assertEqual(ImageCodec.from_exr_code(1), ImageCodec.RLE)
-        self.assertEqual(ImageCodec.from_exr_code(3), ImageCodec.ZIP)
-        self.assertEqual(ImageCodec.from_exr_code(2), ImageCodec.ZIPS)
-        self.assertEqual(
-            ImageCodec.from_exr_code(0),
-            ImageCodec.UNCOMPRESSED
-        )
-
-    def test_from_exr_code_errors(self):
-        expected = 'Value given is not an integer. foo !=.*int'
-        with self.assertRaisesRegex(EnforceError, expected):
-            ImageCodec.from_exr_code('foo')
-
-        expected = 'EXR code 42 has no legal ImageCodec type. '
-        expected += 'Legal EXR codes: .*'
-        with self.assertRaisesRegex(EnforceError, expected):
-            ImageCodec.from_exr_code(42)
 # ------------------------------------------------------------------------------
 
 
