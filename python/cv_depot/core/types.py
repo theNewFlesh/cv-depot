abb161b4fee5cfb00f75825951cfe5c6ff458e02
Alex Braun <alexander.g.braun@gmail.com>
2025-03-21 01:30:00 +0000

    OptArray

--- python/cv_depot/core/types.py
+++ python/cv_depot/core/types.py
@@ -1,5 +1,7 @@
-from typing import Union
+from typing import Optional, Union
 from pathlib import Path
+
+import numpy as np
 
 from cv_depot.core.enum import Anchor
 from cv_depot.core.color import BasicColor, Color
@@ -8,3 +10,4 @@ from cv_depot.core.color import BasicColor, Color
 AnyAnchor = Union[Anchor, str]
 AnyColor = Union[Color, BasicColor, str]
 Filepath = Union[str, Path]
+OptArray = Optional[np.ndarray]
